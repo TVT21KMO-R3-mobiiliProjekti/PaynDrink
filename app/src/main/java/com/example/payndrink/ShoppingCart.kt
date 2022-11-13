d67f70f098dc97e8c0ef8f41b9f04bdab4df4e8d
Teemu Lahtinen <tlahtine@gmail.com>
2022-11-13 12:36:51 +0200

    some bug fixes

--- app/src/main/java/com/example/payndrink/ShoppingCart.kt
+++ app/src/main/java/com/example/payndrink/ShoppingCart.kt
@@ -1,22 +1,13 @@
 package com.example.payndrink
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class ShoppingCart : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scanner)
-
-        /** Skip to main activity */
-        val btnSkip = findViewById<Button>(R.id.btnSkip)
-        btnSkip.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_scanner_sub)
     }
 
 
